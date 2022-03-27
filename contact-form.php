54357aa7605eb86ea572fe7fd20a0c25cdb9606f
Nishant Shedage <76552034+Nish7156@users.noreply.github.com>
2022-03-27 14:01:26 +0530

    rr

--- contact-form.php
+++ contact-form.php
@@ -10,7 +10,7 @@ if(empty($Name) && empty($Email) && empty($Subject) && empty($Message)){
     echo ('Please fill all the fields');
 }
 else{
-    mail("webtriggers11@gmail.com", "Message From webTriggers.com - ".$Subject,"Name: ".$Name."Phone: " .$Phone." Message: " .$Message." Email: ".$Email, $header);
+    mail("info@webtriggers.tech", "Message From webTriggers.com - ".$Subject,"Name: ".$Name."Phone: " .$Phone." Message: " .$Message." Email: ".$Email, $header);
     echo "<script type='text/javascript'> alert('Your Message sent successfully');
     window.location.href='index.html';
             window.history.log(-1);
