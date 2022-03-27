d1d8d0e82246f371ea5e7062a2bb08da4b5b9ff8
Nishant Shedage <76552034+Nish7156@users.noreply.github.com>
2022-03-27 14:06:27 +0530

    hh

--- contact-form.php
+++ contact-form.php
@@ -6,11 +6,11 @@ $Phone = $_REQUEST['phone'];
 $Subject = $_REQUEST['subject'];
 $Message = $_REQUEST['message'];
 $header = $Email;
-if(empty($Name) && empty($Email) && empty($Subject) && empty($Message)){
+if(empty($Name) && empty($Email)){
     echo ('Please fill all the fields');
 }
 else{
-    mail("info@webtriggers.tech", "Message From webTriggers.com - ".$Subject,"Name: ".$Name."Phone: " .$Phone." Message: " .$Message." Email: ".$Email, $header);
+    mail("info@webtriggers.tech", "Message From webTriggers.com - "subject: ".$Subject,"Name: ".$Name."Phone: " .$Phone." Message: " .$Message." Email: ".$Email, $header);
     echo "<script type='text/javascript'> alert('Your Message sent successfully');
     window.location.href='index.html';
             window.history.log(-1);
