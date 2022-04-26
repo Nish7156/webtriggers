8e5fe80f58fb4457a956b4d0b84335506ccfeb30
Nish7156 <shedagenishant40@gmail.com>
2022-04-26 22:00:51 +0530

    new

--- contact-form.php
+++ contact-form.php
@@ -1,17 +1,20 @@
+
 <?php
 
-$Name = $_REQUEST['name'];
-$Email = $_REQUEST['email'];
-$Phone = $_REQUEST['phone'];
-$Message = $_REQUEST['message'];
-$header = $Email;
-if(empty($Name) && empty($Email) && empty($Message)){
-    echo ('Please fill all the fields');
-}
-else{
-    mail("webtriggers11@gmail.com", "Message From webTriggers.com - ".$Subject,"Name: ".$Name."Phone: " .$Phone." Message: " .$Message." Email: ".$Email, $header);
-    echo "alert('Your Message sent successfully')";
-    echo "<meta http-equiv='refresh' content='0'>";
+
+if($_POST["name"]);
+if($_POST["email"]);
+if($_POST["phone"]);
+if($_POST["message"]) {
+
+
+mail("webtriggers11@gmail.com", "Message From webTriggers",
+
+
+$_POST["insert your message here"]. "From: webtriggers11@gmail.com");
+echo "alert('Your Message sent successfully')";
+
+
 }
 
 
