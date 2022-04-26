f3e272fa8ef15aed9cf62c00fdd97a5b96bb8845
Nish7156 <shedagenishant40@gmail.com>
2022-04-26 22:20:25 +0530

    HIII

--- contact-form.php
+++ contact-form.php
@@ -1,6 +1,20 @@
 <?php
-if($_POST["Message"]) {
-mail("shedagenishant40@gmail.com", "xxxxxxxxxxxx",
-$_POST["Insert Your Message"]. "From: NS");
+$Name = $_REQUEST['name'];
+$contact = $_REQUEST['phone'];
+$Email = $_REQUEST['email'];
+$message= $_REQUEST['message'];
+
+
+   if (empty ($Name) || empty($email) || empty($message))
+{
+    echo "Please fill all the fields";
+}
+else{
+    mail("webtriggers11@gmail.com", "MedLab-Ware Message", $message ,"From:$Name <$email>");
+    echo "<script type='text/javascript'> alert('your message sent sucessfully')
+    window.history.log(-1);
+
+    </script>";
+    
 }
 ?>
